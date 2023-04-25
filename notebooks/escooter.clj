aeae33d047611ef4e0fb64f9f1b32cab3756ec73
Dieter Komendera <dieter@komendera.com>
2023-04-25 21:44:02 +0200

    Tweek escooter description

--- notebooks/escooter.clj
+++ notebooks/escooter.clj
@@ -8,7 +8,11 @@
   [:link {:rel "stylesheet"
           :href "https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
           :crossorigin ""}]
-  [:h1.text-xs.text-slate-500 "E-Scooter Beschränkungen Wien"]])
+  [:h1.text-slate-500 "E-Scooter-Beschränkungszonen Wien"]
+  [:div.text-xs.text-slate-700
+   [:p "Darstellung der Wiener E-Scooter-Beschränkungszonen"]
+   [:blockquote.font-mono "Um ein besseres Miteinander im öffentlichen Raum zu gewährleisten, wurden für Sharing-Mobility-Anbieter (z.B. E-Scooter-Anbieter) einige Regeln aufgestellt. In diesem Datensatz werden Zonen definiert, in denen bestimmte Beschränkungen gelten. Es handelt sich dabei um (zeitabhängige) Fahr- und/oder Abstellverbote, Geschwindigkeitsbeschränkungen bzw. Abstellmöglichkeiten. Die Zonen basieren u.a. auf Fußgängerzonen, Begegnungszonen und den Flächen der ständigen Märkte laut Marktordnung."]
+   [:p "Quelle: " [:a {:href "https://www.data.gv.at/katalog/dataset/a454ea84-6b4f-4961-a511-bf47ef6c1bd0#resources"} "https://www.data.gv.at"]]]])
 
 ^{::clerk/visibility {:result :hide}}
 (def location
@@ -22,13 +26,10 @@
 ^{::clerk/visibility {:result :hide}}
 (clerk/eval-cljs-str cljs-code)
 
-
-
 ^{::clerk/visibility {:result :hide}}
 (def leaflet
   {:transform-fn clerk/mark-presented
    :render-fn 'leaflet/escooter-leaflet-render})
-
 
 ^{::clerk/width :full}
 (clerk/with-viewer leaflet
