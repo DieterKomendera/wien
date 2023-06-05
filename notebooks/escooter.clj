7c37516d3bdca959a3bcaec9962d2b2d84d45854
Dieter Komendera <dieter@komendera.com>
2023-06-05 08:38:19 +0200

    Title and content org

--- notebooks/escooter.clj
+++ notebooks/escooter.clj
@@ -3,16 +3,7 @@
   (:require [nextjournal.clerk :as clerk])
   (:import [java.time LocalDate]))
 
-(clerk/html
- [:<>
-  [:link {:rel "stylesheet"
-          :href "https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
-          :crossorigin ""}]
-  [:h1.text-slate-500 "E-Scooter-Beschränkungszonen Wien"]
-  [:div.text-xs.text-slate-700
-   [:p "Darstellung der Wiener E-Scooter-Beschränkungszonen"]
-   [:blockquote.font-mono "Um ein besseres Miteinander im öffentlichen Raum zu gewährleisten, wurden für Sharing-Mobility-Anbieter (z.B. E-Scooter-Anbieter) einige Regeln aufgestellt. In diesem Datensatz werden Zonen definiert, in denen bestimmte Beschränkungen gelten. Es handelt sich dabei um (zeitabhängige) Fahr- und/oder Abstellverbote, Geschwindigkeitsbeschränkungen bzw. Abstellmöglichkeiten. Die Zonen basieren u.a. auf Fußgängerzonen, Begegnungszonen und den Flächen der ständigen Märkte laut Marktordnung."]
-   [:p "Quelle: " [:a {:href "https://www.data.gv.at/katalog/dataset/a454ea84-6b4f-4961-a511-bf47ef6c1bd0#resources"} "https://www.data.gv.at"]]]])
+;; # E-Scooter Beschränkungen Wien
 
 ^{::clerk/visibility {:result :hide}}
 (def location
@@ -35,12 +26,23 @@
 (clerk/with-viewer leaflet
   @location)
 
+
+
 (clerk/html
  [:div
   [:span.font-mono.text-2 "Legende"]
   [:img {:src "https://www.komendera.com/gis/e-scooter-legend.png"
          :width "200"}]])
 
+(clerk/html
+ [:<>
+  [:link {:rel "stylesheet"
+          :href "https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+          :crossorigin ""}]
+  [:div.text-xs.text-slate-700
+   [:p "Darstellung der Wiener E-Scooter-Beschränkungszonen"]
+   [:blockquote.font-mono "Um ein besseres Miteinander im öffentlichen Raum zu gewährleisten, wurden für Sharing-Mobility-Anbieter (z.B. E-Scooter-Anbieter) einige Regeln aufgestellt. In diesem Datensatz werden Zonen definiert, in denen bestimmte Beschränkungen gelten. Es handelt sich dabei um (zeitabhängige) Fahr- und/oder Abstellverbote, Geschwindigkeitsbeschränkungen bzw. Abstellmöglichkeiten. Die Zonen basieren u.a. auf Fußgängerzonen, Begegnungszonen und den Flächen der ständigen Märkte laut Marktordnung."]
+   [:p "Quelle: " [:a {:href "https://www.data.gv.at/katalog/dataset/a454ea84-6b4f-4961-a511-bf47ef6c1bd0#resources"} "https://www.data.gv.at"]]]])
 
 ^::clerk/no-cache
 (clerk/html
